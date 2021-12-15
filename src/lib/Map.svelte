aa12fec9134218ebd48483a1b7bab73ffd0ca8c5
v-rogg <mail@valentinrogg.de>
2021-12-15 10:55:42 +0100

    Simplify

---
+++ src/lib/Map.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+  import { _ } from "$lib/actions/helpers";
+
+  export let mapSVG;
+  export let mapData;
+
+	onMount(async () => {
+    _(`#_${0}-${13}`).style.fill = mapData.map[0][13]["1600"].toString();
+    _(`#_${0}-${4}`).style.fill = mapData.map[0][4]["1600"].toString();
+    _(`#_${0}-${8}`).style.fill = mapData.map[0][8]["1600"].toString();
+  });
+</script>
+
+<section id="map">
+  {#if mapSVG}
+    <div class="vector">
+      {@html mapSVG}
+    </div>
+  {/if}
+</section>
+
+<style lang="sass">
+  @import "../styles/variables"
+
+  #map
+    display: flex
+    align-items: center
+    width: calc(100% - $p-side-width - $p-side-width)
+    background: lightblue
+    position: absolute
+    top: $p-top-spacing
+    bottom: $p-bottom-spacing
+    left: $p-side-width
+    opacity: 1
+
+  .vector
+    width: 100%
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,42 @@
+<script lang="ts" context="module">
+  import type { Load } from "@sveltejs/kit";
+
+  export const load: Load = async ({fetch}) => {
+    let svg, json;
+
+    await fetch(`${import.meta.env.VITE_PUBLIC_BASE_PATH}/map.svg`)
+      .then((res) => res.text())
+      .then((data) => {
+        console.log('svg loaded');
+        svg = data;
+      })
+      .catch((error) => console.log(error));
+
+    await fetch(`${import.meta.env.VITE_PUBLIC_BASE_PATH}/maps.json`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log('json loaded');
+        json = data;
+      })
+      .catch((error) => console.log(error));
+
+    return {
+      props: {
+        mapSVG: svg,
+        mapData: json,
+      }
+    }
+  }
+
+</script>
+
 <script lang="ts">
 	import Left from '$lib/Left.svelte';
 	import Right from '$lib/Right.svelte';
 	import Manipulator from '$lib/Manipulator.svelte';
-  import JS from "$lib/JS.svelte";
+  import Map from "$lib/Map.svelte";
+
+  export let mapSVG, mapData;
 </script>
 
 <svelte:head>
@@ -10,6 +44,6 @@
 </svelte:head>
 
 <Left />
-<JS />
+<Map {mapSVG} {mapData} />
 <Right />
 <Manipulator />
