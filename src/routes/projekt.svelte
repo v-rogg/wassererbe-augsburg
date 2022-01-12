98786995ae2a7c1b8f781067333ef57d2cb44fc9
nadikel <nadine.keller@hs-augsburg.de>
2022-01-12 14:13:39 +0100

    Add Vietnam Font

---
+++ src/routes/projekt.svelte
@@ -0,0 +1,13 @@
+<svelte:head>
+  <title>Über das Projekt – Wassererbe Augsburg</title>
+</svelte:head>
+
+<h1>
+  Über das Projekt
+</h1>
+
+<style lang="sass">
+  @import "src/styles/theme"
+  h1
+    color: $c-blue
+</style>
